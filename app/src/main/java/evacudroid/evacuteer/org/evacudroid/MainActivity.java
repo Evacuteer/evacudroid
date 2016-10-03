78be406c6b4b0b2f1b3f3d04d666034aab320d29
Josh McAdams <joshua.mcadams@gmail.com>
2016-10-03 18:43:03 -0500

    App skeleton

---
+++ app/src/main/java/evacudroid/evacuteer/org/evacudroid/MainActivity.java
@@ -0,0 +1,13 @@
+package evacudroid.evacuteer.org.evacudroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
